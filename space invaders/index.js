ffd6a1485719aa0d7584419682fbe9505ce1a39d
Victor Andersson <victor.andersson@skola.taby.se>
2022-05-09 17:31:36 +0200

    la till rörelse för spelare och typ projektiler

--- space invaders/index.js
+++ space invaders/index.js
@@ -5,11 +5,33 @@ const c = canvas.getContext('2d')
 canvas.width = innerWidth
 canvas.height = innerHeight
 
+class Projectile{
+    constructor(){
+        this.position = {
+            x: 1000,
+            y:850
+        }
+        const image = new Image()
+        image.src = 'img/projectile.png'
+        image.onload = () => {
+        }
+        this.image = image 
+        this.width = image.width
+        this.height = image.height    
+    }
+        draw() {
+            //c.fillStyle = 'red'
+            //c.fillRect(this.position.x, this.position.y, this.width, this.height)
+            c.drawImage(this.image, this.position.x, this.position.y, this.height, this.width)
+        }
+    
+}
+
 class Player {
     constructor(){
         this.position = {
-            x: 200,
-            y: 200
+            x: 1000,
+            y: 850
         }
 
         this.velocity = {
@@ -18,20 +40,21 @@ class Player {
         }
 
         const image = new Image()
-        image.src = './spaceship.png'
+        image.src = 'img/spaceship.png'
         image.onload = () => {
-            
         }
+
         this.image = image 
         this.width = image.width
-        this.height = image.height
+        this.height = image.height    
     }
         draw() {
-        //c.fillStyle = 'red'
-        //c.fillRect(this.position.x, this.position.y, this.width, this.height)
-        c.drawImage(this.image, this.position.x, this.position.y, this.height, this.width)
-    }
+            //c.fillStyle = 'red'
+            //c.fillRect(this.position.x, this.position.y, this.width, this.height)
+            c.drawImage(this.image, this.position.x, this.position.y, this.height, this.width)
+        }
 }
+const projectiles = new Array()
 const player = new Player()
 player.draw()
 
@@ -40,7 +63,27 @@ function animate() {
     c.fillStyle =  'green'
     c.fillRect(0, 0, canvas.width, canvas.height)
     player.draw()
+    projectiles.forEach(element => {
+        element.draw()
+    });
 }
 
 animate()
+
+addEventListener("keydown", (event)=>{
+    switch(event.key){
+        case "a":
+            player.position.x += -100
+            console.log("left")
+            break
+        case "d":
+            player.position.x += 100
+            console.log("right")
+            break
+        case " ":
+            projectiles.push(new Projectile())
+            console.log("space")
+            break
+    }    
+})
 
